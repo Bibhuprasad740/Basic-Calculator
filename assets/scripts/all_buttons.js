b1835dfc435832073771efe5c560f597a5739582
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2023-09-04 00:44:10 +0530

    completed calculator functionalities

--- assets/scripts/all_buttons.js
+++ assets/scripts/all_buttons.js
@@ -1,21 +1,104 @@
-// All buttons
-const oneButton = document.getElementById('one-btn');
-const twoButton = document.getElementById('two-btn');
-const threeButton = document.getElementById('three-btn');
-const fourButton = document.getElementById('four-btn');
-const fiveButton = document.getElementById('five-btn');
-const sixButton = document.getElementById('six-btn');
-const sevenButton = document.getElementById('seven-btn');
-const eightButton = document.getElementById('eight-btn');
-const nineButton = document.getElementById('nine-btn');
-const zeroButton = document.getElementById('zero-btn');
+
+// Numbers
+const oneButton = {
+    'button': document.getElementById('one-btn'),
+    'value': '1',
+    'isNumber': true,
+    'isOperator': false,
+};
+const twoButton = {
+    'button': document.getElementById('two-btn'),
+    'value': '2',
+    'isNumber': true,
+    'isOperator': false,
+};
+
+const threeButton = {
+    'button': document.getElementById('three-btn'),
+    'value': '3',
+    'isNumber': true,
+    'isOperator': false,
+};
+const fourButton = {
+    'button': document.getElementById('four-btn'),
+    'value': '4',
+    'isNumber': true,
+    'isOperator': false,
+};
+const fiveButton = {
+    'button': document.getElementById('five-btn'),
+    'value': '5',
+    'isNumber': true,
+    'isOperator': false,
+};
+const sixButton = {
+    'button': document.getElementById('six-btn'),
+    'value': '6',
+    'isNumber': true,
+    'isOperator': false,
+};
+const sevenButton = {
+    'button': document.getElementById('seven-btn'),
+    'value': '7',
+    'isNumber': true,
+    'isOperator': false,
+};
+const eightButton = {
+    'button': document.getElementById('eight-btn'),
+    'value': '8',
+    'isNumber': true,
+    'isOperator': false,
+};
+const nineButton = {
+    'button': document.getElementById('nine-btn'),
+    'value': '9',
+    'isNumber': true,
+    'isOperator': false,
+};
+const zeroButton = {
+    'button': document.getElementById('zero-btn'),
+    'value': '0',
+    'isNumber': true,
+    'isOperator': false,
+}
+const dotButton = {
+    'button': document.getElementById('dot-btn'),
+    'value': '.',
+    'isNumber': false,
+    'isOperator': false,
+}
+
+// Operators
+const plusButton = {
+    'button': document.getElementById('plus-btn'),
+    'value': '+',
+    'isNumber': false,
+    'isOperator': true,
+};
+const minusButton = {
+    'button': document.getElementById('minus-btn'),
+    'value': '-',
+    'isNumber': false,
+    'isOperator': true,
+}
+const multiplyButton = {
+    'button': document.getElementById('multiply-btn'),
+    'value': '*',
+    'isNumber': false,
+    'isOperator': true,
+}
+const divideButton = {
+    'button': document.getElementById('divide-btn'),
+    'value': '/',
+    'isNumber': false,
+    'isOperator': true,
+}
+
+// Utility Buttons
 const deleteButton = document.getElementById('del-btn');
 const resetButton = document.getElementById('reset-btn');
 const equaltoButton = document.getElementById('equalto-btn');
-const plusButton = document.getElementById('plus-btn');
-const minusButton = document.getElementById('minus-btn');
-const multiplyButton = document.getElementById('multiply-btn');
-const divideButton = document.getElementById('divide-btn');
+
 
 // button checker
 // multiplyButton.addEventListener('click', function () {
